70df07ed014d1a87b24ab4b51166a2bc6d144728
Aleksandr Shelukhin <mad.di.s@yandex.ru>
2022-10-02 18:36:51 +0300

    Сверстать адаптировать секцию о студенте и портфолио

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import aboutMeImage from "../../images/aboutme-photo.jpg";
+import Portfolio from "../Portfolio/Portfolio";
 
 const AboutMe = () => {
   return (
-    <div>
-
-    </div>
+    <section className="about-me">
+      <div className="container">
+        <h2 className="section__title">Студент</h2>
+        <hr className="section__line"/>
+        <div className="about-me__wrapper">
+          <div className="about-me__col">
+            <p className="about-me__title">Александр</p>
+            <p className="about-me__subtitle">Немолодой Фронтенд-разработчик</p>
+            <p className="about-me__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut cumque distinctio, ea eum excepturi explicabo, illum iure nostrum officia sit tempore. Accusantium incidunt ipsam minus molestias nemo provident suscipit. Architecto aut cumque distinctio, ea eum excepturi explicabo, illum iure nostrum officia sit tempore. Accusantium incidunt.</p>
+            <Link to="https://github.com/Aleksandr-Shelukhin" className="about-me__link transition-on-hover">Github</Link>
+          </div>
+          <img src={aboutMeImage} alt="Фотография студента" className="about-me__image"/>
+        </div>
+        <Portfolio/>
+      </div>
+    </section>
   );
 };
 
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Portfolio = () => {
   return (
-    <div>
-
-    </div>
+    <section className="portfolio">
+      <h3 className="portfolio__title">Портфолио</h3>
+      <ul className="portfolio__links">
+        <li className="portfolio__item">
+          <Link to="#" className="portfolio__link transition-on-hover">Статичный сайт</Link>
+        </li>
+        <li className="portfolio__item">
+          <Link to="#" className="portfolio__link transition-on-hover">Адаптивный сайт</Link>
+        </li>
+        <li className="portfolio__item">
+          <Link to="#" className="portfolio__link transition-on-hover">Одностраничное приложение</Link>
+        </li>
+      </ul>
+    </section>
   );
 };
 
