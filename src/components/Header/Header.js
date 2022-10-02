3768393c4abac73700bd114d8549ccf6d1688c95
Aleksandr Shelukhin <mad.di.s@yandex.ru>
2022-10-02 13:21:19 +0300

    Сверстать и адаптировать шапку

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import {Link, Route} from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <p>Header</p>
+    <div className="header">
+      <Link to='/'>
+        <div className="header__logo"></div>
+      </Link>
+        <nav className="header__nav">
+          <Route path='/movie'>
+            <Link to='/movies' className="header__link header__link_active transition-on-hover">Фильмы</Link>
+            <Link to='/saved-movies' className="header__link transition-on-hover">Сохранённые фильмы</Link>
+          </Route>
+        </nav>
+        <div className="header__links">
+          <Route exact path='/'>
+            <Link to='/signup' className="header__link header__link_type_signup transition-on-hover">Регистрация</Link>
+            <Link to='/signin' className="header__link header__link_type_signin transition-on-hover">Войти</Link>
+          </Route>
+          <Route path='/movie'>
+            <Link to='/profile' className="header__link header__link_type_profile transition-on-hover">Аккаунт</Link>
+            <div className="header__burger transition-on-hover">
+              <div className="header__burger-line"></div>
+            </div>
+          </Route>
+        </div>
     </div>
   );
 };
