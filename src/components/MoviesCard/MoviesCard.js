e50151fff02b2d1b2e798c7cf28e1e919879fdb9
Aleksandr Shelukhin <mad.di.s@yandex.ru>
2022-10-03 14:31:24 +0300

    Сделать общую страницу с фильмами

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import cardImage from '../../images/movies/pic__COLOR_pic.jpg'
 
 const MoviesCard = () => {
   return (
-    <div>
-
+    <div className="card">
+      <img src={cardImage} alt="Постер" className="card__image"/>
+      <div className="card__title-wrapper">
+        <div className="card__title">33&nbsp;слова о&nbsp;дизайне</div>
+        <button className="card__button transition-on-hover"></button>
+      </div>
+      <p className="card__duration">1ч42м</p>
     </div>
   );
 };
